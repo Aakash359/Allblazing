f3eda0d35b049dcfd8fece6558224d5ac7df8f89
Tarun Bhardwaj <tarun.bhardwaj.@quytech.com>
2021-02-25 20:26:45 +0530

    joi group, request group, accept group request, reject group request, withdraw group request, map screen design

--- src/components/chat-groups.js
+++ src/components/chat-groups.js
@@ -1,78 +1,89 @@
-
-import React from 'react';
-import { bool, func, shape } from 'prop-types';
-import { TouchableOpacity, Text, View, Image } from 'react-native';
-import Constants from '../constants';
-import { InviteFriendsStyles } from '../styles';
+import React from 'react'
+import {bool, func, shape} from 'prop-types'
+import {TouchableOpacity, Text, View, Image} from 'react-native'
+import Constants from '../constants'
+import {InviteFriendsStyles} from '../styles'
 
 export const ChatGroup = ({
-  hasChats,
-  navigation,
-  group,
-  type
-
-}) => { 
-
-  const getMembers = () => {
-    let members = group?.userInfo?.userData || []
-    let nameMembers = members.filter(i => i?.full_name)
-    let getThreeMembers = nameMembers.slice(0, nameMembers.length > 3 ? 3 : nameMembers.length).map(mem => mem?.full_name)
-    let res = ''
-    getThreeMembers.map(name => res += `${name.split(' ')[0]}, `)
-    res = res.slice(0, -2) + ' '
-    if(nameMembers.length) {
-      if(members.length > 3){
-      
-        return res += `and ${members.length - getThreeMembers.length} others`
-      }
-      else
-        return `${res}`
+    hasChats,
+    navigation,
+    group,
+    type,
+    requestType = false,
+}) => {
+    const getMembers = () => {
+        let members = group?.userInfo?.userData || []
+        let nameMembers = members.filter((i) => i?.full_name)
+        let getThreeMembers = nameMembers
+            .slice(0, nameMembers.length > 3 ? 3 : nameMembers.length)
+            .map((mem) => mem?.full_name)
+        let res = ''
+        getThreeMembers.map((name) => (res += `${name.split(' ')[0]}, `))
+        res = res.slice(0, -2) + ' '
+        if (nameMembers.length) {
+            if (members.length > 3) {
+                return (res += `and ${
+                    members.length - getThreeMembers.length
+                } others`)
+            } else return `${res}`
+        } else {
+            return members.length + ' others'
+        }
     }
-    else {
-      return members.length + ' others'
-    }
-  }
 
-  const members = getMembers()
+    const members = getMembers()
 
-  // React.useEffect(() => {
-  //   console.log(getMembers())
-  // }, [group])
-  
-  return (
-    <TouchableOpacity
-      activeOpacity={0.7}
-      onPress={() => navigation('GroupDetail',
-        { groupId: group?.group_id, group, type })}
-      style={[InviteFriendsStyles.container, {height: 80}]}
-    >
-    <View style={[InviteFriendsStyles.userWrapper, {maxWidth: '70%'}]}>
-      <Image source={Constants.Images.groupDetails} style={InviteFriendsStyles.userImage} />
-      <View >
-        <Text style={InviteFriendsStyles.username}>{group?.name}</Text>
-        <View >
-        <Text numberOfLines={2} ellipsizeMode="tail" style={InviteFriendsStyles.location}>{members}</Text>
-        </View>
-      </View>
-    </View>
-    {hasChats && (
-      <View style={InviteFriendsStyles.chatCount}>
-        <Text style={InviteFriendsStyles.chatText}>1</Text>
-      </View>
-    )}
+    const image = group?.image
+        ? group?.image === 'N/A'
+            ? Constants.Images.groupDetails
+            : {uri: group?.image}
+        : Constants.Images.groupDetails
 
-  </TouchableOpacity>
-);
+    return (
+        <TouchableOpacity
+            activeOpacity={0.7}
+            onPress={() =>
+                navigation('GroupDetail', {
+                    groupId: group?.group_id,
+                    group,
+                    type,
+                    requestType,
+                })
+            }
+            style={[InviteFriendsStyles.container, {height: 80}]}>
+            <View style={[InviteFriendsStyles.userWrapper, {maxWidth: '70%'}]}>
+                <Image source={image} style={InviteFriendsStyles.userImage} />
+                <View>
+                    <Text style={InviteFriendsStyles.username}>
+                        {group?.name}
+                    </Text>
+                    <View>
+                        <Text
+                            numberOfLines={2}
+                            ellipsizeMode="tail"
+                            style={InviteFriendsStyles.location}>
+                            {members}
+                        </Text>
+                    </View>
+                </View>
+            </View>
+            {hasChats && (
+                <View style={InviteFriendsStyles.chatCount}>
+                    <Text style={InviteFriendsStyles.chatText}>1</Text>
+                </View>
+            )}
+        </TouchableOpacity>
+    )
 }
 
 ChatGroup.propTypes = {
-  hasChats: bool,
-  navigation: shape({
-    navigate: func,
-    setParams: func,
-  }).isRequired,
-};
+    hasChats: bool,
+    navigation: shape({
+        navigate: func,
+        setParams: func,
+    }).isRequired,
+}
 
-ChatGroup.defaultProps = { hasChats: true };
+ChatGroup.defaultProps = {hasChats: true}
 
-export default ChatGroup;
+export default ChatGroup
--- src/styles/group-detail-styles.js
+++ src/styles/group-detail-styles.js
@@ -1,26 +1,26 @@
-import { StyleSheet, Dimensions } from 'react-native';
-import Constants from '../constants';
+import {StyleSheet, Dimensions} from 'react-native'
+import Constants from '../constants'
 
-const { width } = Dimensions.get('window');
+const {width} = Dimensions.get('window')
 
 export default StyleSheet.create({
-  container: {
-    backgroundColor: Constants.Colors.PRIMARY,
-    flex: 1,
-  },
-  nextText: {
-    ...Constants.Fonts.Regular,
-    color: Constants.Colors.WHITE,
-    fontSize: Constants.BaseStyle.scale(16),
-    textAlign: 'center',
-  },
-  nextView: {
-    backgroundColor: Constants.Colors.TEXT_COLOR2,
-    // marginBottom: height * 0.05,
-    marginHorizontal: 10,
-    marginTop: 20,
-    padding: 15,
-    width: width * 0.9,
-  },
-
-});
+    container: {
+        backgroundColor: Constants.Colors.PRIMARY,
+        flex: 1,
+    },
+    nextText: {
+        ...Constants.Fonts.Regular,
+        color: Constants.Colors.WHITE,
+        fontSize: Constants.BaseStyle.scale(16),
+        textAlign: 'center',
+    },
+    nextView: {
+        backgroundColor: Constants.Colors.TEXT_COLOR2,
+        // marginBottom: height * 0.05,
+        marginHorizontal: 10,
+        marginTop: 20,
+        padding: 15,
+        width: width * 0.9,
+        borderRadius: 3,
+    },
+})
