e904131f44848a587eddfcdfa4a39a7573b5fe3a
Tarun Bhardwaj <tarun.bhardwaj.@quytech.com>
2021-03-01 19:42:13 +0530

    fix user login, fix intro, fix save login cred on login, event details

--- src/routes/bottom-tabs-stack.js
+++ src/routes/bottom-tabs-stack.js
@@ -1,75 +1,92 @@
-import * as React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { useTranslation } from 'react-i18next';
-import { BottomTab } from '../components';
-import Constants from '../constants';
-import { BottomTabsStyles } from '../styles';
-import ChatStack from './chat-stack';
-import CreatePostStack from './create-post-stack';
-import DiscoverStack from './discover-stack';
-import HomeNavigator from './home-stack';
-import ProfileStack from './profile-stack';
-import {PlusIconPopup} from '../components';
-const Tab = createBottomTabNavigator();
+import * as React from 'react'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import {useTranslation} from 'react-i18next'
+import {BottomTab} from '../components'
+import Constants from '../constants'
+import {BottomTabsStyles} from '../styles'
+import ChatStack from './chat-stack'
+import CreatePostStack from './create-post-stack'
+import DiscoverStack from './discover-stack'
+import HomeNavigator from './home-stack'
+import ProfileStack from './profile-stack'
+import {PlusIconPopup} from '../components'
+const Tab = createBottomTabNavigator()
 
 const tabBarOptions = {
-  activeBackgroundColor: Constants.Colors.SECONDARY_COLOR,
-  activeTintColor: Constants.Colors.WHITE,
-  inactiveBackgroundColor: Constants.Colors.SECONDARY_COLOR,
-  inactiveTintColor: Constants.Colors.SECONDARY_COLOR,
-  keyboardHidesTabBar: true,
-  labelPosition: 'below-icon',
-  labelStyle: BottomTabsStyles.label,
-  showLabel: true,
-  style: BottomTabsStyles.tab,
-};
+    activeBackgroundColor: Constants.Colors.SECONDARY_COLOR,
+    activeTintColor: Constants.Colors.WHITE,
+    inactiveBackgroundColor: Constants.Colors.SECONDARY_COLOR,
+    inactiveTintColor: Constants.Colors.SECONDARY_COLOR,
+    keyboardHidesTabBar: true,
+    labelPosition: 'below-icon',
+    labelStyle: BottomTabsStyles.label,
+    showLabel: true,
+    style: BottomTabsStyles.tab,
+}
 
 const tabProps = {
-  backBehavior: 'history',
-  initialRouteName: 'Home',
-  lazy: true,
-  sceneContainerStyle: BottomTabsStyles.container,
-  tabBarOptions,
-};
+    backBehavior: 'history',
+    initialRouteName: 'Home',
+    lazy: true,
+    sceneContainerStyle: BottomTabsStyles.container,
+    tabBarOptions,
+}
 const Dummy = () => {
-  return null;
-};
+    return null
+}
 const TabNavigator = (props) => {
-  const { t } = useTranslation();
-  const [visible, seVisible] = React.useState(false)
-  console.log("PPORRSSSS", props.navigation);
-  return (
-    <Tab.Navigator {...tabProps} tabBar={(props) => <BottomTab {...props} />}>
-      <Tab.Screen options={{ tabBarLabel: t('Home') }} name="Home" component={HomeNavigator} />
-      <Tab.Screen options={{ tabBarLabel: t('Discover') }} name="Discover" component={DiscoverStack} />
-      <Tab.Screen 
-      listeners={({ navigation, route }) => ({
-        tabPress: e => {
-          e.preventDefault();
-          
-          //navigation.navigate("EntryFormFlow");
-        },
-      })}
-      options={{ 
-        tabBarLabel: t('Create'), 
-        tabBarButton:()=>{
-          return(  <Text style = {{color:'red'}}>HI</Text>)
-       
-          // return( <PlusIconPopup
-          //   // onFacebook={() => this.setState({visible: false})}
-            
-          //   // onWhatsApp={() => this.setState({visible: false})}
-          //   onClose={() => seVisible(!visible)}
-          // />)
-        }  
-    }} 
-        name="Create"
-        component={() => null}
-      />
-      <Tab.Screen options={{ tabBarLabel: t('Chat') }} name="Chat" component={ChatStack} />
-      <Tab.Screen options={{ tabBarLabel: t('Me') }} name="Me" component={ProfileStack} />
-    </Tab.Navigator>
-  );
-};
+    const {t} = useTranslation()
+    const [visible, seVisible] = React.useState(false)
+    return (
+        <Tab.Navigator
+            {...tabProps}
+            tabBar={(props) => <BottomTab {...props} />}>
+            <Tab.Screen
+                options={{tabBarLabel: t('Home')}}
+                name="Home"
+                component={HomeNavigator}
+            />
+            <Tab.Screen
+                options={{tabBarLabel: t('Discover')}}
+                name="Discover"
+                component={DiscoverStack}
+            />
+            <Tab.Screen
+                listeners={({navigation, route}) => ({
+                    tabPress: (e) => {
+                        e.preventDefault()
 
-export default TabNavigator;
+                        //navigation.navigate("EntryFormFlow");
+                    },
+                })}
+                options={{
+                    tabBarLabel: t('Create'),
+                    tabBarButton: () => {
+                        return <Text style={{color: 'red'}}>HI</Text>
+
+                        // return( <PlusIconPopup
+                        //   // onFacebook={() => this.setState({visible: false})}
+
+                        //   // onWhatsApp={() => this.setState({visible: false})}
+                        //   onClose={() => seVisible(!visible)}
+                        // />)
+                    },
+                }}
+                name="Create"
+                component={() => null}
+            />
+            <Tab.Screen
+                options={{tabBarLabel: t('Chat')}}
+                name="Chat"
+                component={ChatStack}
+            />
+            <Tab.Screen
+                options={{tabBarLabel: t('Me')}}
+                name="Me"
+                component={ProfileStack}
+            />
+        </Tab.Navigator>
+    )
+}
+
+export default TabNavigator
--- src/screens/create/create-post.js
+++ src/screens/create/create-post.js
@@ -1,230 +1,302 @@
-import React, {Component} from 'react';
-import {View, Image, Text, TouchableOpacity, SafeAreaView} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
-import {CreatePostStyles} from '../../styles';
-import Constants from '../../constants';
-import ImagePicker from 'react-native-image-crop-picker';
-import axios from 'axios';
-import API from '../../constants/baseApi';
-import {getAuthToken, getOtpToken} from '../../helpers/auth';
-import {Alert} from 'react-native';
-import {connect} from 'react-redux';
-import {withTranslation} from 'react-i18next';
-import {func, shape} from 'prop-types';
-import {ActivityIndicator} from 'react-native';
+import React, {Component} from 'react'
+import {View, Image, Text, TouchableOpacity, SafeAreaView} from 'react-native'
+import {useNavigation} from '@react-navigation/native'
+import {CreatePostStyles} from '../../styles'
+import Constants from '../../constants'
+import ImagePicker from 'react-native-image-crop-picker'
+import axios from 'axios'
+import API from '../../constants/baseApi'
+import {getAuthToken, getOtpToken, setUserId} from '../../helpers/auth'
+import {Alert} from 'react-native'
+import {connect} from 'react-redux'
+import {withTranslation} from 'react-i18next'
+import {func, shape} from 'prop-types'
+import {ActivityIndicator} from 'react-native'
+import {setProfileDetails} from '../../reducers/baseServices/profile'
 
 class CreatePost extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: false,
-      upload: false,
-      selectedImage: '',
-    };
-  }
-  // const navigation = useNavigation();
-  // const [upload, setUpload] = useState(false);
-  // const [selectedImage, setSelectedImage] = useState();
-
-  choosePhotosFromGallery = (imageIndex) => {
-    ImagePicker.openPicker({
-      width: 300,
-      height: 400,
-      cropping: false,
-      includeBase64: true,
-    }).then((image) => {
-      // setUpload(true);
-      this.setState({upload: true});
-      // setSelectedImage(image.path);
-      this.setState({selectedImage: image.path});
-      console.log(image.path, imageIndex);
-    });
-  };
-
-  //  takePhotoFromCamera = async (imageIndex) => {
-  //   try {
-  //     const image = await ImagePicker.openCamera({
-  //       cropping: true,
-  //       includeBase64: true,
-  //     });
-  //   } catch (err) {
-  //     // logError(err, '[useProfilePhotos] getPhotoLibrary Error');
-  //   }
-  // };
-  takePhotoFromCamera = (imageIndex) => {
-    try {
-      ImagePicker.openCamera({
-        cropping: false,
-        includeBase64: true,
-      }).then((image) => {
-        this.setState({upload: true});
-        this.setState({selectedImage: image.path});
-        console.log(image.path, imageIndex);
-      });
-    } catch (err) {
-      console.log('error==>', err);
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoading: false,
+            upload: false,
+            selectedImage: '',
+            user: {},
+            Loading: false,
+        }
     }
-  };
+    // const navigation = useNavigation();
+    // const [upload, setUpload] = useState(false);
+    // const [selectedImage, setSelectedImage] = useState();
 
-  _handlePost = async () => {
-    const {
-      navigation: {navigate},
-    } = this.props;
+    choosePhotosFromGallery = (imageIndex) => {
+        ImagePicker.openPicker({
+            width: 300,
+            height: 400,
+            cropping: false,
+            includeBase64: true,
+        }).then((image) => {
+            // setUpload(true);
+            this.setState({upload: true})
+            // setSelectedImage(image.path);
+            this.setState({selectedImage: image.path})
+            console.log(image.path, imageIndex)
+        })
+    }
 
-    // navigate('Feed');
-    // return
-
-    const token = await getAuthToken();
-    console.log('====?', token);
-    const config = {
-      headers: {Authorization: `Bearer ${token}`},
-    };
-
-    const formData = new FormData();
-    formData.append('post', {
-      uri:
-        Platform.OS === 'android'
-          ? this.state.selectedImage
-          : this.state.selectedImage.replace('file://', ''),
-      name: 'post' + '.png',
-      type: 'image/png',
-    });
-    console.log("POST CRETAE FORM DATA", formData);
-    this.setState({
-      isLoading: true,
-    });
-    axios
-      .post(API.POST, formData, config)
-      .then((response) => {
-        console.log('CRETAE POST response' + JSON.stringify(response));
-        if (response?.data?.code === 422) {
-          Alert.alert('', response?.data?.message ?? '');
+    //  takePhotoFromCamera = async (imageIndex) => {
+    //   try {
+    //     const image = await ImagePicker.openCamera({
+    //       cropping: true,
+    //       includeBase64: true,
+    //     });
+    //   } catch (err) {
+    //     // logError(err, '[useProfilePhotos] getPhotoLibrary Error');
+    //   }
+    // };
+    takePhotoFromCamera = (imageIndex) => {
+        try {
+            ImagePicker.openCamera({
+                cropping: false,
+                includeBase64: true,
+            }).then((image) => {
+                this.setState({upload: true})
+                this.setState({selectedImage: image.path})
+                console.log(image.path, imageIndex)
+            })
+        } catch (err) {
+            console.log('error==>', err)
         }
-        if (response?.data?.code === 200) {
-          Alert.alert(
-            '',
-            response?.data?.message ?? '',
-            [
-              {
-                text: 'Cancel',
-                onPress: () => console.log('Cancel pressed'),
-                style: 'Cancel',
-              },
-              {
-                text: 'OK',
-                onPress: () => navigate('Discover'),
-              },
-            ],
-            {Cancelable: false},
-          );
+    }
 
-          // navigate('Discover');
+    UserProfileDetails = async () => {
+        const {user_id, addProfileDetail} = this.props
+        console.log('userid==>', user_id)
+        const token = await getAuthToken()
+        // console.log('====>', token);
+        const config = {
+            headers: {Authorization: `Bearer ${token}`},
         }
-      })
-      .finally(() => {
         this.setState({
-          isLoading: false,
-        });
-      });
-  };
-  render() {
-    // const {nam} = this.state;
+            Loading: true,
+        })
+        axios
+            .post(
+                API.PROFILE_DETAILS,
+                {
+                    user_id: user_id,
+                },
+                config
+            )
+            .then((response) => {
+                console.log('RESPONSE', response)
+                console.log('===>My Profile', response)
+                if (response.data.data.result) {
+                    this.setState({user: response?.data?.data?.result})
+                    addProfileDetail(response?.data?.data?.result)
+                    setUserId(response?.data?.data?.result?.user_id.toString())
+                    console.log(
+                        'profile response==>',
+                        response?.data?.data?.result
+                    )
+                }
+            })
+            .finally(() => {
+                this.setState({
+                    Loading: false,
+                })
+            })
+    }
 
-    const {
-      navigation: {goBack, navigate},
-      route: {params},
-    } = this.props;
-    return (
-      <SafeAreaView style={CreatePostStyles.container}>
-        <View style={CreatePostStyles.userView}>
-          <Image
-            source={Constants.Images.user}
-            style={CreatePostStyles.userIcon}
-          />
-          <Text style={CreatePostStyles.imageText}>Kelly Norman</Text>
-        </View>
+    _handlePost = async () => {
+        const {
+            navigation: {navigate},
+        } = this.props
 
-        {this.state.upload ? (
-          <View>
-            <Image
-              source={{uri: this.state.selectedImage}}
-              style={CreatePostStyles.postImage}
-            />
-            <TouchableOpacity
-              activeOpacity={0.7}
-              onPress={() => this.setState({upload: false})}
-              style={CreatePostStyles.closeIconView}>
-              <Image
-                source={Constants.Images.closeRound}
-                style={CreatePostStyles.closeIcon}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity
-              activeOpacity={0.7}
-              disabled={this.state.isLoading}
-              // onPress={() => navigation.navigate('Feed')}
-              onPress={() => this._handlePost()}
-              style={CreatePostStyles.nextView}>
-              {this.state.isLoading ? (
-                <ActivityIndicator color="white" size={25}/>
-              ) : (
-                <Text style={CreatePostStyles.nextText}>Post</Text>
-              )}
-            </TouchableOpacity>
-          </View>
-        ) : (
-          <View>
-            <TouchableOpacity
-              activeOpacity={0.7}
-              onPress={() => this.takePhotoFromCamera()}
-              style={CreatePostStyles.optionView}>
-              <View style={CreatePostStyles.optionIconView}>
-                <Image
-                  source={Constants.Images.camera}
-                  style={CreatePostStyles.optionIcon}
-                />
-              </View>
-              <Text style={CreatePostStyles.imageText}>Open Camera</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              activeOpacity={0.7}
-              // onPress={() => setUpload(true)}
-              onPress={() => this.choosePhotosFromGallery()}
-              style={CreatePostStyles.optionView}>
-              <View style={CreatePostStyles.optionIconView}>
-                <Image
-                  source={Constants.Images.imageGallery}
-                  style={CreatePostStyles.optionIcon}
-                />
-              </View>
-              <Text style={CreatePostStyles.imageText}>Open Gallery</Text>
-            </TouchableOpacity>
-          </View>
-        )}
-      </SafeAreaView>
-    );
-  }
+        // navigate('Feed');
+        // return
+
+        const token = await getAuthToken()
+        console.log('====?', token)
+        const config = {
+            headers: {Authorization: `Bearer ${token}`},
+        }
+
+        const formData = new FormData()
+        formData.append('post', {
+            uri:
+                Platform.OS === 'android'
+                    ? this.state.selectedImage
+                    : this.state.selectedImage.replace('file://', ''),
+            name: 'post' + '.png',
+            type: 'image/png',
+        })
+        console.log('POST CRETAE FORM DATA', formData)
+        this.setState({
+            isLoading: true,
+        })
+        axios
+            .post(API.POST, formData, config)
+            .then((response) => {
+                console.log('CRETAE POST response' + JSON.stringify(response))
+                if (response?.data?.code === 422) {
+                    Alert.alert('', response?.data?.message ?? '')
+                }
+                if (response?.data?.code === 200) {
+                    Alert.alert(
+                        '',
+                        response?.data?.message ?? '',
+                        [
+                            {
+                                text: 'Cancel',
+                                onPress: () => console.log('Cancel pressed'),
+                                style: 'Cancel',
+                            },
+                            {
+                                text: 'OK',
+                                onPress: () => navigate('Discover'),
+                            },
+                        ],
+                        {Cancelable: false}
+                    )
+
+                    // navigate('Discover');
+                }
+            })
+            .finally(() => {
+                this.setState({
+                    isLoading: false,
+                })
+            })
+    }
+
+    componentDidMount() {
+        this.UserProfileDetails()
+    }
+
+    render() {
+        // const {nam} = this.state;
+
+        const {
+            navigation: {goBack, navigate},
+            route: {params},
+        } = this.props
+        const {user} = this.state
+        const userImage =
+            !user?.image || user?.image === 'N/A'
+                ? Constants.Images.tabBarProfile
+                : {uri: user?.image}
+        return (
+            <SafeAreaView style={CreatePostStyles.container}>
+                <View style={CreatePostStyles.userView}>
+                    <Image
+                        source={userImage}
+                        style={[
+                            CreatePostStyles.userIcon,
+                            {
+                                borderRadius: 23,
+                                borderWidth: 1,
+                                borderColor: Constants.Colors.LIGHT_RED,
+                            },
+                        ]}
+                    />
+                    <Text style={CreatePostStyles.imageText}>
+                        {user?.full_name}
+                    </Text>
+                </View>
+
+                {this.state.upload ? (
+                    <View>
+                        <Image
+                            source={{uri: this.state.selectedImage}}
+                            style={CreatePostStyles.postImage}
+                        />
+                        <TouchableOpacity
+                            activeOpacity={0.7}
+                            onPress={() => this.setState({upload: false})}
+                            style={CreatePostStyles.closeIconView}>
+                            <Image
+                                source={Constants.Images.closeRound}
+                                style={CreatePostStyles.closeIcon}
+                            />
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            activeOpacity={0.7}
+                            disabled={this.state.isLoading}
+                            // onPress={() => navigation.navigate('Feed')}
+                            onPress={() => this._handlePost()}
+                            style={CreatePostStyles.nextView}>
+                            {this.state.isLoading ? (
+                                <ActivityIndicator color="white" size={25} />
+                            ) : (
+                                <Text style={CreatePostStyles.nextText}>
+                                    Post
+                                </Text>
+                            )}
+                        </TouchableOpacity>
+                    </View>
+                ) : (
+                    <View>
+                        <TouchableOpacity
+                            activeOpacity={0.7}
+                            onPress={() => this.takePhotoFromCamera()}
+                            style={CreatePostStyles.optionView}>
+                            <View style={CreatePostStyles.optionIconView}>
+                                <Image
+                                    source={Constants.Images.camera}
+                                    style={CreatePostStyles.optionIcon}
+                                />
+                            </View>
+                            <Text style={CreatePostStyles.imageText}>
+                                Open Camera
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            activeOpacity={0.7}
+                            // onPress={() => setUpload(true)}
+                            onPress={() => this.choosePhotosFromGallery()}
+                            style={CreatePostStyles.optionView}>
+                            <View style={CreatePostStyles.optionIconView}>
+                                <Image
+                                    source={Constants.Images.imageGallery}
+                                    style={CreatePostStyles.optionIcon}
+                                />
+                            </View>
+                            <Text style={CreatePostStyles.imageText}>
+                                Open Gallery
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            </SafeAreaView>
+        )
+    }
 }
 
 // export default CreatePost;
 
 CreatePost.propTypes = {
-  loginSuccess: func.isRequired,
-  navigation: shape({
-    dispatch: func.isRequired,
-    goBack: func.isRequired,
-  }).isRequired,
-  t: func.isRequired,
-};
+    loginSuccess: func.isRequired,
+    navigation: shape({
+        dispatch: func.isRequired,
+        goBack: func.isRequired,
+    }).isRequired,
+    t: func.isRequired,
+}
 
-const mapStateToProps = () => ({});
+const mapStateToProps = ({auth: {user_id}}) => {
+    return {
+        user_id,
+    }
+}
 
 const mapDispatchToProps = {
-  // addFullName: (params) => setFullName(params),
-  // addCreateGroupDetail: (params) => setCreateGroupDetails(params),
-};
+    // addFullName: (params) => setFullName(params),
+    // addCreateGroupDetail: (params) => setCreateGroupDetails(params),
+    addProfileDetail: (params) => setProfileDetails(params),
+}
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(withTranslation()(CreatePost));
+    mapStateToProps,
+    mapDispatchToProps
+)(withTranslation()(CreatePost))
